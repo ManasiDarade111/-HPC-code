b63ce075240199687c213c33b7b63729c16c80d4
Manasi Darade <104726010+ManasiDarade111@users.noreply.github.com>
2024-04-24 01:58:38 +0530

    Update posneg.py

--- posneg.py
+++ posneg.py
@@ -1,3 +1,8 @@
+pip install tensorflow
+pip install keras
+
+pip install keras --upgrade
+
 from keras.datasets import imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -14,6 +19,7 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
 
 
+
 import numpy as np
 
 def vectorize_sequences(sequences, dimension=10000):
@@ -28,12 +34,12 @@ x_train = vectorize_sequences(train_data)
 # Our vectorized test data
 x_test = vectorize_sequences(test_data)
 
+
+
 x_train[0]
 
-# Our vectorized labels
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
-
 
 from keras import models
 from keras import layers
@@ -43,9 +49,11 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
+
 
 
 x_val = x_train[:10000]
@@ -53,7 +61,6 @@ partial_x_train = x_train[10000:]
 
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
-
 
 
 
@@ -65,5 +72,6 @@ history = model.fit(partial_x_train,
 
 
 results = model.evaluate(x_test, y_test)
+
 
 results
